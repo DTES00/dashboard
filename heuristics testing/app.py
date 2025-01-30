84b43dc69924e8b7b7883ab4ecabf59e52afa522
DTES00 <daaneshuis.t@gmail.com>
2025-01-30 18:46:03 +0100

    Add files via upload

--- heuristics testing/app.py
+++ heuristics testing/app.py
@@ -313,13 +313,13 @@ if df is not None and not st.session_state.get("distance_matrix_generated", Fals
             "unique_name": "Universal_Depot"
         })
 
-        # #Local OSRM if available:
+        # # UNOMMENT FOR Local OSRM if available:
         # if profile == "driving":
         #     base_url = "http://localhost:5000"
         # elif profile == "cycling":
         #     base_url = "http://localhost:5001"
         
-        #to public OSRM if you'd like:
+        #COMMENT BELOW AND UNCOMMENT ABOVE FOR LOCAL OSRM:
         base_url = "http://router.project-osrm.org"
 
         dm = create_distance_matrix(
